f8744d6d7de937c0aa7f29c827a7a1b362a98a3e
potapovDim <potapovd3012@gmail.com>
2017-11-28 22:36:07 +0200

    rm typescript app

--- react-ts/src/components/styles.tsx
+++
@@ -1,6 +0,0 @@
-import { style } from 'typestyle';
-
-export const styleH1 = style({
-  color: 'red',
-  borderBottom: '1px dashed green'
-});
--- react-ts/src/reducers/items/constans.tsx
+++
@@ -1,6 +0,0 @@
-export const ADD_ITEM = 'ADD_ITEM';
-export type ADD_ITEM = typeof ADD_ITEM;
-
-export interface StoreState {
-  items: Array<any>;
-}
