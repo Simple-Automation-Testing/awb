5f3507a6d88acd8bf1eac5674b2bf81d4176208a
potapovDim <potapovd3012@gmail.com>
2017-12-30 13:16:00 +0200

    add keys export

--- interface.js
+++ interface.js
@@ -1,10 +1,12 @@
 const { elements, element } = require('./interface/element')
 const browser = require('./interface/client')
+const { Keys } = require('./interface/event/keys')
 
 const { spawnStandalone, killProc } = require('./installer')
 
 module.exports = {
     element,
     elements,
+    Keys,
     client: browser
 }
