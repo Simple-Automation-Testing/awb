8f5fb37fbf5fe73b3995ec041abf2f73fa883715
potapovDim <potapovd3012@gmail.com>
2018-02-22 21:52:37 +0200

    fix

--- awb.js
+++ awb.js
@@ -1,5 +1,5 @@
-const { Keys } = require('./interface/event/keys')
-const awb = require('./interface/client')
+const { Keys } = require('./awb/event/keys')
+const awb = require('./awb/client')
 
 module.exports = awb
 module.exports.Keys = Keys
