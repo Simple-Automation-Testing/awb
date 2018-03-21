5de5a58bd2136e6c76160422ea571d22148f5f12
Potapov Dmitriy <potapovd3012@gmail.com>
2018-03-21 20:29:37 +0200

    fix element elements

--- a.js
+++ a.js
@@ -1,6 +1,20 @@
-
-const arr = [1, 2, 3, 4, 56, 6, 76,]
-
-
-const b = ['x', 'y', ...arr]
-console.log(b)
+function spawnStandalone() {
+  const stPath = 'C:/Users/IEUser/Downloads/selenium-server-standalone-3.11.0.jar'
+  const iePath = 'C:/Users/IEUser/Downloads/IEDriverServer.exe'
+  return new Promise((resolve, reject) => {
+    try {
+      const nodeProc = spawn('java', [
+        `-Dwebdriver.ie.driver=${iePath}`,
+        '-jar',
+        `${stPath}`,
+        '-port',
+        '10259'
+      ], {
+          stdio: ['pipe', process.stdout, process.stderr]
+        })
+      resolve(nodeProc)
+    } catch (error) {
+      console.error(error.toString())
+    }
+  })
+}
