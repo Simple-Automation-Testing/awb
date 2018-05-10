fd2d56bdd7abab9ef1ff8724caa2eb6bfff57ab4
potapovDim <potapovd3012@gmail.com>
2018-05-10 18:30:49 +0300

    rm util

--- __specs__/util.js
+++
@@ -1,9 +0,0 @@
-// const FakeServer = require('test-fake-server')
-
-// const fakeServer = new FakeServer(8085)
-
-// fakeServer.get('/bar', { bar: 'bar' })
-
-// module.exports = {
-//   fakeServer
-// }
