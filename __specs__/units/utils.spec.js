c2230b1a5e15dbb3adf5d38532bfc2656c04764c
potapovDim <potapovd3012@gmail.com>
2018-05-12 12:16:06 +0300

    rm useless tests

---
+++ __specs__/units/utils.spec.js
@@ -0,0 +1,24 @@
+const {baseNegativeWait, waitCondition, waitElementPresent} = require('../../lib/util')
+const {expect} = require('chai')
+
+describe('Wait conditions', () => {
+  const getCondition = () => {
+    let requester = 0
+    return async () => {
+      requester += 1
+      if(requester < 5) {return {status: 0}}
+      else {return {status: 1}}
+    }
+  }
+  it('baseNegativeWait time', async () => {
+    const conditionFunction = getCondition()
+    const resp = await baseNegativeWait(conditionFunction, 2500, 'error')
+    expect(resp).to.have.property('time')
+  })
+
+  it('baseNegativeWait error', async () => {
+    const conditionFunction = getCondition()
+    const resp = await baseNegativeWait(conditionFunction, 50, 'error')
+    expect(resp).to.have.property('error')
+  })
+})
