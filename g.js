4e4cab0b9b8e5b143c3caea2133597378bcea5fb
potapovDim <potapovd3012@gmail.com>
2018-07-10 16:55:58 +0300

    utio

--- g.js
+++ g.js
@@ -1,16 +1,3 @@
-const arrayToSubArrays = (arr, formedArr = [], arrLenth = 8) => {
-  if(arr.length >= arrLenth) {
-    formedArr.push(arr.splice(0, 8))
-
-    return arrayToSubArrays(arr, formedArr, arrLenth)
-
-  } else if(arr.length < arrLenth) {
-    formedArr.push(arr.splice(0, arr.length))
-  }
-  return formedArr
-}
-
-
 const sort = `
 function bubbleSort(a) {
   let swapped
